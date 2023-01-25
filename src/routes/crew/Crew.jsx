0df40c78946a7b62061d1bdae6f044ec81e89989
Nolger Rodriguez <nolgerluisrodriguezmedina@gmail.com>
2023-01-25 13:31:00 -0500

    arrgleos 3 pagina sin responsivo

--- src/routes/crew/Crew.jsx
+++ src/routes/crew/Crew.jsx
@@ -5,16 +5,21 @@ import imgDouglas from "../../assets/crew/image-douglas-hurley.png"; // importo 
 import imgMark from "../../assets/crew/image-mark-shuttleworth.png"; // importo las imagenes de mark
 import imgVictor from "../../assets/crew/image-victor-glover.png"; // importo las imagenes de victor
 import imgAnousheh from "../../assets/crew/image-anousheh-ansari.png"; // importo las imagenes de anousheh
+import { useScreen } from "../../hooks/useScreen"; // importo el hook para saber el ancho de la pantalla
 
 const Crew = () => {
   const [select, setSelect] = useState(0); // creo el estado para seleccionar el miembro del equipo
   const img = [imgDouglas, imgMark, imgVictor, imgAnousheh]; // creo un array con las imagenes
+  const changeCrew = (index) => () => setSelect(index); // creo una funcion para cambiar el estado
+  const width = useScreen().width; // creo una variable para saber el ancho de la pantalla
+  console.log(width)
   return (
     <div className={style.CrewContainer}>
       <div className={style.Crew}>
         <h1 className={style.TittleCrew}>
           <p className={style.SubTittleCrew}>02 </p>meet your crew
         </h1>
+        {width < 768 ? (console.log("resposivo")):(console.log("no responsivo"))}
         <div className={style.CrewInfoContainer}>
           <div className={style.CrewLeftContainer}>
             <div className={style.InfoCrewContainer}>
@@ -23,10 +28,10 @@ const Crew = () => {
               <h3 className={style.BioCrew}>{data.crew[select].bio}</h3>
             </div>
             <div className={style.CrewBtnContainer}>
-              <button className={style.CrewBtnSelected}></button>
-              <button className={style.CrewBtn}></button>
-              <button className={style.CrewBtn}></button>
-              <button className={style.CrewBtn}></button>
+              <button onClick={changeCrew(0)} className={select == 0 ? style.CrewBtnSelected : style.CrewBtn}></button>
+              <button onClick={changeCrew(1)} className={select == 1 ? style.CrewBtnSelected : style.CrewBtn}></button>
+              <button onClick={changeCrew(2)} className={select == 2 ? style.CrewBtnSelected : style.CrewBtn}></button>
+              <button onClick={changeCrew(3)} className={select == 3 ? style.CrewBtnSelected : style.CrewBtn}></button>
             </div>
           </div>
           <div className={style.CrewRigthContainer}>
